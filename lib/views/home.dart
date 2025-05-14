240a90d4258c4f10be297be9ea72871a94134d52
eliseo.nava <eliseo.nava@cbtis128.edu.mx>
2025-05-14 09:48:19 +0000

    first commit

---
+++ lib/views/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/model/data_producto.dart';
+import 'package:myapp/widgets/banner.dart';
+import 'package:myapp/widgets/custom_app_bar.dart';
+import 'package:myapp/widgets/product_tile.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const CustomAppBar(), // custom_app_bar.dart';
+           
+            Expanded(
+              child: ListView(
+                children: [
+                   const MainBanner(),//banner.dart';
+                  ListView.builder(
+                    shrinkWrap: true,
+                    primary: false,
+                    itemCount: products.length,
+                    itemBuilder:
+                        (context, index) =>
+                            ProdcutTile(product: products[index]),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
